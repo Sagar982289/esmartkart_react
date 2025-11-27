9f3c770590b10b6609d36dec96c91ad53bb2f555
Sagar Shende <sagar9889@gmail.com>
2025-11-27 12:58:37 +0530

    Add files via upload

---
+++ src/utils/staticCredential.js
@@ -0,0 +1,7 @@
+export const google_client_id =
+  "704408774585-pc8lfnkstp48ssrm238tmt9ge0lead69.apps.googleusercontent.com";
+export const fb_app_id = "2033465357423036";
+export const appleLoginCredential = {
+  serviceId: "",
+  redirectURI: ``,
+};
